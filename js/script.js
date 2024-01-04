fda6ed0e562dfac27f47a57e49679749fbac41c4
md saydur rahoman <145344840+saydurrahoman@users.noreply.github.com>
2024-01-04 03:41:52 -1200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.number').easy_number_animate({
+        start_value: 0,
+        end_value: 5000,
+        duration: 5000,
+        delimiter: '10000,',
+        before: null,
+        after: null
+      });
+});
